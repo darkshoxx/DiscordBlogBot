dd48d932b9ede5ed1fefa7eb71d184670e68a6d3
darkshoxx <43265727+darkshoxx@users.noreply.github.com>
2023-12-24 11:32:23 +0000

    Create fake repo_secrets.py
    needed for imports during testing

---
+++ DiscordBlogBot/repo_secrets.py
@@ -0,0 +1,12 @@
+# This is a surrogate file containing example code. Required for testing.
+BOT_TOKEN = "dslgkafhgliyuasdrbfdiuksehgtiukaweshfgviudsrfhtawe"
+CHANNEL_ID = 8279346709674985306
+BLOGS = [{
+  'author': 'CoolAuthor',
+  'url': 'https://public-api.wordpress.com/wp/v2/sites/blog.com'
+}]
+NON_WP_BLOGS = [{
+  'author': 'AnotherCoolAuthor',
+  'url': 'https://bloggieblog.com',
+  'update_route': '/news'
+}]
